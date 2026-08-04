ae6265cab03841dbbe75ef15b7c2b80eb4e9f3d4
Nattan <natanshadow2004@gmail.com>
2026-08-04 14:58:07 -0300

    Clase 02 - Agrego estructura JS y primer ejemplo de asincronia

---
+++ JS/asincronia.js
@@ -0,0 +1,43 @@
+// console.log("Inicio")
+// setTimeout(() =>{
+//     console.log("Buscando alumnos")
+// }, 3000)
+
+// console.log("Fin")
+
+// function saludar(){
+//     console.log("Hola")
+// }
+
+// function ejecutar(funcion){
+//     funcion();
+// }
+
+// ejecutar(saludar)
+
+
+// function despedirse(){
+//     console.log("Hasta luego")
+// }
+
+// setTimeout(despedirse, 3000)
+
+
+// setTimeout(()=>{
+//     console.log("Buscando docentes...")
+// },2000)
+// setTimeout(()=>{
+//     console.log("Buscando materias...")
+// },4000)
+// setTimeout(()=>{
+//     console.log("Buscando cursos...")
+// },1000)
+
+
+console.log("Abriendo SGA")
+setTimeout(()=>{
+    console.log("Alumnos cargados")
+
+},3000)
+
+console.log("El usuario puede seguir navegando")
