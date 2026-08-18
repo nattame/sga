1f27b75d695d59ce53c50989200874ffcc763786
Nattan <natanshadow2004@gmail.com>
2026-08-18 15:21:13 -0300

    Clase 09 - fontawesome y confirmacion de eliminacion

--- JS/alumnos.js
+++ JS/alumnos.js
@@ -251,8 +251,14 @@ function mostrarAlumnos(alumnos){
         <td>${alumno.carrera}</td>
         <td>${alumno.correo}</td>
         <td>
-            <button class="btn-editar" data-id="${alumno.id}">Editar</button>
-            <button class="btn-eliminar" data-id="${alumno.id}">Eliminar</button>
+            <button class="btn-editar" 
+            data-id="${alumno.id}"
+            title="Editar alumno">
+            <i class="fa-solid fa-pen"></i></button>
+            <button class="btn-eliminar" data-id="${alumno.id}"
+            title="Eliminar alumno">
+            <i class="fa-solid fa-trash"></i>
+            </button>
 
 
         </tr>
@@ -270,18 +276,27 @@ function eliminarAlumno(id){
 
     localStorage.setItem("alumnos", JSON.stringify(alumnosActualizados))
     mostrarAlumnos(alumnosActualizados)
+    if(alumnoEditandoId===id){
+        formulario.reset()
+        alumnoEditandoId = null;
+        formulario.querySelector("button").textContent = "Guardar alumno";
+    }
     mostrarMensaje("Alumno eliminado correctamente", "mje-exito")
 }
 
 
 listaAlumnos.addEventListener("click",(e)=>{
-    if(e.target.classList.contains("btn-eliminar")){
-        const id = Number(e.target.dataset.id)
-        eliminarAlumno(id)
+    const boton_el = e.target.closest(".btn-eliminar") 
+    if(boton_el){
+        const id = Number(boton_el.dataset.id)
+        const confirmar = confirm("Estas seguro de eliminar este alumno?")
+        if(confirmar){
+            eliminarAlumno(id)
+        }
     }
-
-    if(e.target.classList.contains("btn-editar")){
-        const id = Number(e.target.dataset.id)
+    const boton_ed = e.target.closest(".btn-editar")
+    if(boton_ed){
+        const id = Number(boton_ed.dataset.id)
         editarAlumno(id)
     }
 })
@@ -294,6 +309,7 @@ function editarAlumno(id){
     document.querySelector("#correo").value = alumno.correo;
     alumnoEditandoId = id;
     formulario.querySelector("button").textContent = "Actualizar Alumno"
+    document.querySelector("#nombre").focus()
 }
 
 const alumnos = obtenerAlumnos()
