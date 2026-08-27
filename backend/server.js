db1dbbb1987c9fbec9be2d21f1144ed4e07f7613
Nattan <natanshadow2004@gmail.com>
2026-08-27 15:56:12 -0300

    Trabajamos con express y peticion GET

---
+++ backend/server.js
@@ -0,0 +1,106 @@
+const express = require("express");
+
+const app = express()
+
+
+const alumnos = [
+    {
+        id: 1,
+        nombre: "Axel",
+        carrera: "Sistemas"
+    },
+    {
+        id: 2,
+        nombre: "Ailen",
+        carrera: "Gastronomia"
+
+    },
+    {
+        id: 3,
+        nombre: "Brian",
+        carrera: "Redes"
+    },
+    {
+        id: 4,
+        nombre: "Camila",
+        carrera: "???"
+    },
+    {
+        id: 5,
+        nombre: "Milagros",
+        carrera: "???"
+    }
+]
+
+const docentes = [
+    {
+        id: 1,
+        nombre: "Sergio",
+        carrera: "Sistemas"
+    },
+    {
+        id: 2,
+        nombre: "Manuel",
+        carrera: "Peluqueria"
+
+    },
+    {
+        id: 3,
+        nombre: "Diego",
+        carrera: "Ingenieria"
+    },
+    {
+        id: 4,
+        nombre: "Zahira",
+        carrera: "???"
+    },
+    {
+        id: 5,
+        nombre: "Nena",
+        carrera: "Perrita profesional"
+    }
+]
+
+
+
+
+
+app.get("/alumnos", (req,res)=>{
+    res.json(alumnos)
+})
+
+app.get("/docentes",(req,res)=>{
+    res.json(docentes)
+})
+
+app.get("/docentes/:id",(req,res)=>{
+    const id = Number(req.params.id)
+    const docente = docentes.find(a => a.id === id)
+    res.json(docente)
+})
+
+
+
+
+app.get("/alumnos/:id", (req,res)=>{
+    const id = Number(req.params.id)
+    const alumno = alumnos.find(a => a.id === id)
+    res.json(alumno)
+})
+
+
+app.listen(3000, ()=>{
+    console.log("Servidor funcionando en http://localhost:3000")
+})
+
+
+
+//crear GET /alumnos
+
+//responder al menos 5 alumnos
+
+//Crear GET /alumnos/:id
+
+//Probar ambas rutas con navegador y postman.
+
+// Consultar un ID inexistente y oservar que ocurre.
